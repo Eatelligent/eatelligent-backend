8ecdf163fe7ade27c39e187791965fd69cd38f9e
sigurlu <sigurd.l@gmail.com>
2014-10-01 10:16:51 +0200

    spray version

---
+++ src/main/scala/com/mealchooser/database/PgSprayJsonSupport.scala
@@ -0,0 +1,29 @@
+package com.mealchooser.database
+
+import scala.slick.driver.PostgresDriver
+import scala.slick.lifted.Column
+import scala.slick.jdbc.JdbcType
+
+trait PgSprayJsonSupport extends json.PgJsonExtensions with utils.PgCommonJdbcTypes { driver: PostgresDriver =>
+  import spray.json._
+  import DefaultJsonProtocol._ // !!! IMPORTANT, otherwise `convertTo` and `toJson` won't work correctly.
+
+  trait JsonImplicits {
+    implicit val jsonTypeMapper =
+      new GenericJdbcType[JsValue](
+        "json",
+        (s) => s.parseJson,
+        (v) => v.toJson.compactPrint,
+        hasLiteralForm = false
+      )
+
+    implicit def jsonColumnExtensionMethods(c: Column[JsValue])(
+      implicit tm: JdbcType[JsValue], tm1: JdbcType[List[String]]) = {
+      new JsonColumnExtensionMethods[JsValue, JsValue](c)
+    }
+    implicit def jsonOptionColumnExtensionMethods(c: Column[Option[JsValue]])(
+      implicit tm: JdbcType[JsValue], tm1: JdbcType[List[String]]) = {
+      new JsonColumnExtensionMethods[JsValue, Option[JsValue]](c)
+    }
+  }
+}
--- src/main/scala/com/mealchooser/database/Schema.scala
+++ src/main/scala/com/mealchooser/database/Schema.scala
@@ -2,8 +2,10 @@ package com.mealchooser.database
 
 import MyPostgresDriver.simple._
 import com.github.tminglei.slickpg.JsonString
-import org.json4s.native.Json
+import org.threeten.bp.ZonedDateTime
 import spray.http.DateTime
+import org.joda.time.LocalDateTime
+import spray.json.JsValue
 import scala.slick.lifted.{ProvenShape, ForeignKeyQuery}
 
 
@@ -16,19 +18,22 @@ class Language(tag: Tag) extends Table[(Int, String, String)](tag, "language") {
     (id, locale, name)
 }
 
-//class Recipe(tag: Tag) extends Table[(Int, String, JsonString, String, Int, Double, String, DateTime, DateTime)](tag,
-//  "recipe") {
-//  def id: Column[Int] = column[Int]("id", O.PrimaryKey)
-//  def name: Column[String] = column[String]("name")
-//  def image: Column[JsonString] = column[JsonString]("image")
-//  def description: Column[String] = column[String]("descritption")
-//  def language: Column[Int] = column[Int]("language")
-//  def calories: Column[Double] = column[Double]("calories")
-//  def procedure: Column[String] = column[String]("procedure")
-//  def created: Column[DateTime] = column[DateTime]("created")
-//  def modified: Column[DateTime] = column[DateTime]("modified")
-//
-//  def * : ProvenShape[(Int, String, JsonString, String, Int, Double, String, DateTime, DateTime)] =
-//    (id, name, image, description, language, calories, procedure, created, modified)
-//}
+class Recipe(tag: Tag) extends Table[(Int, String, Option[JsValue], String, Int, Double, String,
+  Option[LocalDateTime],
+  Option[LocalDateTime])](tag,
+  "recipe") {
+  def id: Column[Int] = column[Int]("id", O.PrimaryKey)
+  def name: Column[String] = column[String]("name")
+  def image: Column[Option[JsValue]] = column[Option[JsValue]]("image")
+  def description: Column[String] = column[String]("descritption")
+  def language: Column[Int] = column[Int]("language")
+  def calories: Column[Double] = column[Double]("calories")
+  def procedure: Column[String] = column[String]("procedure")
+  def created: Column[Option[LocalDateTime]] = column[Option[LocalDateTime]]("created")
+  def modified: Column[Option[LocalDateTime]] = column[Option[LocalDateTime]]("modified")
+
+  def * : ProvenShape[(Int, String, Option[JsValue], String, Int, Double, String, Option[LocalDateTime],
+    Option[LocalDateTime])] =
+    (id, name, image, description, language, calories, procedure, created, modified)  <> (JsonBean.tupled, JsonBean.unapply)
+}
 
