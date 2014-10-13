2b2f9adcb0f5c76e5ea46f32be44ef709945e428
sigurlu <sigurd.l@gmail.com>
2014-10-13 15:10:04 +0200

    changed inserts to query the references instead

--- database/data.sql
+++ database/data.sql
@@ -7,9 +7,9 @@ VALUES
 
 INSERT INTO users (name, password, email, role)
 VALUES
-  ('bone', 'bone', 'tandeey@gmail.com', 2),
-  ('gurr', 'gurr', 'sigurd.l@gmail.com', 2),
-  ('garr', 'garr', 'pelle@pelle.com', 1);
+  ('bone', 'bone', 'tandeey@gmail.com', (SELECT id from roles WHERE name = 'admin')),
+  ('gurr', 'gurr', 'sigurd.l@gmail.com', (SELECT id from roles WHERE name = 'admin')),
+  ('garr', 'garr', 'pelle@pelle.com', (SELECT id from roles WHERE name = 'user'));
 
 INSERT INTO ingredient(name)
 VALUES
@@ -31,23 +31,23 @@ VALUES
 
 INSERT INTO recipe(name, description, language, calories, procedure, created, modified)
 VALUES
-  ('Lasser', 'En optimal lasser', 1, 1000, 'Hvordan lager man en lasser? Jo.. Nå skal du høre..', Now(), Now());
+  ('Lasser', 'En optimal lasser', (SELECT id from language WHERE name = 'Norwegian'), 1000, 'Hvordan lager man en lasser? Jo.. Nå skal du høre..', Now(), Now());
 
 INSERT INTO ingredient_in_recipe(recipe_id, ingredient_id, amount)
 VALUES
-  (1, 1, 400),
-  (1, 2, 300),
-  (1, 3, 200),
-  (1, 4, 1),
-  (1, 5, 1);
+  ((SELECT id from recipe WHERE name = 'Lasser'), (SELECT id from ingredient WHERE name = 'Kjøttdeig'), 400),
+  ((SELECT id from recipe WHERE name = 'Lasser'), (SELECT id from ingredient WHERE name = 'Melk'), 300),
+  ((SELECT id from recipe WHERE name = 'Lasser'), (SELECT id from ingredient WHERE name = 'Vann'), 200),
+  ((SELECT id from recipe WHERE name = 'Lasser'), (SELECT id from ingredient WHERE name = 'Lasagne plater'), 1),
+  ((SELECT id from recipe WHERE name = 'Lasser'), (SELECT id from ingredient WHERE name = 'Lasagne krydder'), 1);
 
-INSERT INTO user_star_rate_meal(user_id, meal_id, stars)
+INSERT INTO user_star_rate_recipe(user_id, recipe_id, stars)
 VALUES
-  (3, 1, 5.0);
+  ((SELECT id from users WHERE name = 'garr'), (SELECT id from recipe WHERE name = 'Lasser'), 5.0);
 
 INSERT INTO user_yes_no_rate_ingredient(user_id, ingredient_id, rating)
 VALUES
-  (3, 1, true);
+  ((SELECT id from users WHERE name = 'garr'), (SELECT id from recipe WHERE name = 'Lasser'), true);
 
 
 
