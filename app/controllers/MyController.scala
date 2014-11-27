0fa9fa86f7188b0bbf4c139b0e7b4e6ef9ba111d
sigurlu <sigurd.l@gmail.com>
2014-11-27 13:46:15 +0100

    Read author from cookie instead of json

--- app/controllers/MyController.scala
+++ app/controllers/MyController.scala
@@ -74,39 +74,6 @@ class MyController extends Controller {
 //      (JsPath \ "name").write[String]
 //    )(unlift(Unit.unapply))
 
-
-  implicit val recipeRead: Reads[Recipe] = (
-    (JsPath \ "id").readNullable[Long] and
-      (JsPath \ "name").read[String] and
-      (JsPath \ "image").readNullable[String] and
-      (JsPath \ "description").read[String] and
-      (JsPath \ "language").read[Int] and
-      (JsPath \ "calories").readNullable[Double] and
-      (JsPath \ "procedure").read[String] and
-      (JsPath \ "spicy").read[Int] and
-      (JsPath \ "created").readNullable[DateTime] and
-      (JsPath \ "modified").readNullable[DateTime] and
-      (JsPath \ "ingredients").read[Seq[IngredientForRecipe]] and
-      (JsPath \ "tags").read[Seq[String]] and
-      (JsPath \ "createdBy").read[TinyUser]
-    )(Recipe.apply _)
-
-  implicit val recipeWrites: Writes[Recipe] = (
-    (JsPath \ "id").write[Option[Long]] and
-      (JsPath \ "name").write[String] and
-      (JsPath \ "image").write[Option[String]] and
-      (JsPath \ "description").write[String] and
-      (JsPath \ "language").write[Int] and
-      (JsPath \ "calories").write[Option[Double]] and
-      (JsPath \ "procedure").write[String] and
-      (JsPath \ "spicy").write[Int] and
-      (JsPath \ "created").write[Option[DateTime]] and
-      (JsPath \ "modified").write[Option[DateTime]] and
-      (JsPath \ "ingredients").write[Seq[IngredientForRecipe]] and
-      (JsPath \ "tags").write[Seq[String]] and
-      (JsPath \ "createdBy").write[TinyUser]
-    )(unlift(Recipe.unapply))
-
   implicit val tinyRecipeRead: Reads[TinyRecipe] = (
     (JsPath \ "id").read[Long] and
       (JsPath \ "name").read[String] and
