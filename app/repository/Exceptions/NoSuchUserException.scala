71e8420193a6682f0e606dbf1f077e8a06e60aaa
sigurlu <sigurd.l@gmail.com>
2015-01-20 13:17:09 +0100

    Missing file

---
+++ app/repository/Exceptions/NoSuchUserException.scala
@@ -0,0 +1,9 @@
+package repository.Exceptions
+
+case class NoSuchUserException(message: String) extends  Exception(message) {
+  def this(id: Long) = this("No user with id: " + id + " was found.")
+}
+
+object NoSuchUserException {
+  def apply(id: Option[Long]) = new NoSuchUserException("No user with id: " + id + " was found.")
+}
