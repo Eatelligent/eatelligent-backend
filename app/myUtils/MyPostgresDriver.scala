a7506a9813078e81c886d02080f1412352f32c4d
sigurlu <sigurd.l@gmail.com>
2015-02-03 11:48:25 +0100

    removed some unecessary stuff from the driver

--- app/myUtils/MyPostgresDriver.scala
+++ app/myUtils/MyPostgresDriver.scala
@@ -2,35 +2,27 @@ package myUtils
 
 import com.github.tminglei.slickpg._
 import slick.driver.PostgresDriver
-import com.github.tototoshi.slick.{GenericJodaSupport, PostgresJodaSupport}
 
 trait WithMyDriver {
   val driver: MyPostgresDriver
-//  val jodaSupport: GenericJodaSupport
 }
 
-////////////////////////////////////////////////////////////
 trait MyPostgresDriver extends PostgresDriver
-with PgArraySupport
-with PgDateSupportJoda
-with PgRangeSupport
-with PgSearchSupport
-with PgPlayJsonSupport {
+  with PgArraySupport
+  with PgDateSupportJoda
+  with PgRangeSupport
+  with PgSearchSupport
+  with PgPlayJsonSupport {
 
-  override lazy val Implicit = new ImplicitsPlus {}
   override val simple = new SimpleQLPlus {}
 
-  //////
-  trait ImplicitsPlus extends Implicits
-  with ArrayImplicits
-  with DateTimeImplicits
-  with RangeImplicits
-  with JsonImplicits
-  with SearchImplicits
-
   trait SimpleQLPlus extends SimpleQL
-  with ImplicitsPlus
-  with SearchAssistants
+    with SearchAssistants
+    with ArrayImplicits
+    with DateTimeImplicits
+    with RangeImplicits
+    with JsonImplicits
+    with SearchImplicits
 }
 
 object MyPostgresDriver extends MyPostgresDriver
